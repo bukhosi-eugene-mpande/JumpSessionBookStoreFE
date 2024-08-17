91c07f30b7dd54ece73ee431db3352de5067a6fd
KNtsako <Khuzwayo.nk@gmail.com>
2024-08-17 14:15:35 +0200

    login page

--- jumpsession-fe/src/app/Components/login/login.component.ts
+++ jumpsession-fe/src/app/Components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import Login from 'src/app/Models/login';
 import { LoginService } from 'src/app/Services/login.service';
 import { NavService } from 'src/app/Shared-Services/nav.service';
 
@@ -19,27 +20,32 @@ export class LoginComponent {
   //ToDo: stop users from navigating without login 
 
   Login() {
-    // this.loginService.login(this.username, this.password).subscribe(
-    //   success => {
-    //     if (success) {
-    //       this.router.navigateByUrl('/home');
-    //     } else {
-    //       this.errorMessage = 'Invalid username or password';
-    //     }
-    //   },
-    //   error => {
-    //     this.errorMessage = 'An error occurred. Please try again.';
-    //   }
-    // );
-    this.loginService.login(this.username, this.password).subscribe({
-      next: () => {
-        this.router.navigateByUrl('/users');
-        this.navService.toggleNav();
+    let loginUser: Login = {
+      username: this.username,
+      password: this.password
+    }
+    this.loginService.login(loginUser).subscribe(
+      success => {
+        if (success) {
+          this.router.navigateByUrl('/users');
+        } else {
+          this.errorMessage = 'Invalid username or password';
+        }
       },
-      error: () => {
+      error => {
+        
         this.errorMessage = 'An error occurred. Please try again.';
       }
-    });
+    );
+    // this.loginService.login(this.username, this.password).subscribe({
+    //   next: () => {
+    //     this.router.navigateByUrl('/users');
+    //     this.navService.toggleNav();
+    //   },
+    //   error: () => {
+    //     this.errorMessage = 'An error occurred. Please try again.';
+    //   }
+    // });
 
   }
 }
